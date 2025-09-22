3ef2e3ca70fe87e9ec5e7b23fd267e1fd4bf48a6
Rahma260 <rahmakh248@gmail.com>
2025-09-22 22:09:31 +0300

    update routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import { Toaster } from "react-hot-toast";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicProfile from "./pages/PublicProfile";
 import ProjectDetails from "./pages/ProjectDetails";
+import Settings from "./pages/Settings";
 function App() {
 
   return (
