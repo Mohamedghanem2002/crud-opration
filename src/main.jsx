cf0a9b5f4c1eec3de1769d68e88b2d2afc801fca
Mohamed Ghanem <mg0447837@gmail.com>
2025-09-24 20:35:56 +0300

    Fix Redux import path case sensitivity

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import "./i18n";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store } from "./Redux/store";
 
 Modal.setAppElement("#root");
 ReactDOM.createRoot(document.getElementById("root")).render(
